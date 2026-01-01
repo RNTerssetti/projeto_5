a9093ca35444d308c3789b570d2ef3a04937e4ec
rafaelterssetti <rafaelterssetti@gmail.com>
2026-01-01 11:53:44 -0300

    inclusão botão de seleção historigrama

--- app.py
+++ app.py
@@ -17,3 +17,11 @@ if hist_button: # se o botão for clicado
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
+
+
+build_histogram = st.checkbox('Criar um histograma')
+
+if build_histogram: # se a caixa de seleção for selecionada
+    st.write('Criando um histograma para a coluna model_year')
+    fig = px.histogram(df, x="model_year")
+    st.plotly_chart(fig, use_container_width=True)
