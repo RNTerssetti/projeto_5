e1b02b387c71435c38467e54c9834d9bf2f54e84
rafaelterssetti <rafaelterssetti@gmail.com>
2026-01-01 11:58:29 -0300

    att

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-"""Pelo menos um cabeçalho. Você pode usar [st.header()] para fazer isso. Na lição sobre aplicativos web, mostramos como criar um cabeçalho.
-Um botão que, ao ser clicado, cria um histograma plotly-express. Para fazer isso, considere usar as funções st.write() e st.plotly_chart() (os materiais estão em inglês). Aqui está um exemplo de como você pode fazer isso:"""
-
+st.title('Análise de Anúncios de Vendas de Carros Usados')
 df = pd.read_csv("datasets/vehicles.csv")
 hist_button = st.button('Criar histograma') # criar um botão
         
@@ -22,6 +20,13 @@ if hist_button: # se o botão for clicado
 build_histogram = st.checkbox('Criar um histograma')
 
 if build_histogram: # se a caixa de seleção for selecionada
+
     st.write('Criando um histograma para a coluna model_year')
+
     fig = px.histogram(df, x="model_year")
+    fig2 = px.histogram(df, x="price")
+
     st.plotly_chart(fig, use_container_width=True)
+    
+
+st.header('Análise de Anúncios de Vendas de Carros Usados')
