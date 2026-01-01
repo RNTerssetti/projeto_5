3e719092ec44bcca495072895294f172f7562e7d
rafaelterssetti <rafaelterssetti@gmail.com>
2026-01-01 12:42:14 -0300

    incluindo novos botões

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-
-st.title('Análise de Anúncios de Vendas de Carros Usados')
 df = pd.read_csv("datasets/vehicles.csv")
+st.header('Análise de Anúncios de Vendas de Carros Usados')
+
 hist_button = st.button('Criar histograma') # criar um botão
         
 if hist_button: # se o botão for clicado
@@ -17,16 +17,32 @@ if hist_button: # se o botão for clicado
     st.plotly_chart(fig, use_container_width=True)
 
 
+if hist_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um grafico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+    
+    # criar um histograma
+    fig = px.scatter(df, x="model_year", y="price")
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
 build_histogram = st.checkbox('Criar um histograma')
+build_scatter = st.checkbox('Criar um gráfico de dispersão')
 
 if build_histogram: # se a caixa de seleção for selecionada
 
     st.write('Criando um histograma para a coluna model_year')
 
     fig = px.histogram(df, x="model_year")
-    fig2 = px.histogram(df, x="price")
+    st.plotly_chart(fig, use_container_width=True)
 
+if build_scatter: # se a caixa de seleção for selecionada
+
+    st.write('Criando um gráfico de dispersão para as colunas model_year e price')
+
+    fig = px.scatter(df, x="model_year", y="price")
     st.plotly_chart(fig, use_container_width=True)
     
-
-st.header('Análise de Anúncios de Vendas de Carros Usados')
